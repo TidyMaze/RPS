d632082db5812ff33c05f8cc5ccef7fb70588b29
yrolland <rolland_yann@orange.fr>
2020-11-29 01:10:58 +0100

    wip

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "RPS"
+
+version := "0.1"
+
+scalaVersion := "2.13.4"
+
+libraryDependencies ++= Seq(
+  "org.scala-graph" %% "graph-core" % "1.13.2",
+  "org.scala-graph" %% "graph-dot" % "1.13.0"
+)
---
+++ src/main/scala/tuto/App.scala
@@ -0,0 +1,73 @@
+package tuto
+
+import scalax.collection.Graph
+import scalax.collection.GraphPredef._
+import scalax.collection.GraphEdge._
+import scalax.collection.edge.Implicits._
+import scalax.collection.edge.WDiEdge
+import scalax.collection.io.dot._
+import implicits._
+
+import scala.util.Random
+
+sealed trait RPS extends Product with Serializable
+
+case object R extends RPS
+
+case object P extends RPS
+
+case object S extends RPS
+
+object RPS {
+  val allRPS = List(R, P, S)
+}
+
+object App {
+  type Node = Seq[RPS]
+
+  val random = new Random
+
+  def subSequences[A](items: Seq[A]): Seq[Seq[A]] = items match {
+    case Seq(single) => Seq(Seq(single))
+    case Seq(fst, others@_*) => (fst +: others) +: subSequences(others)
+  }
+
+  def showNode(n: Node) = n match {
+    case Nil => "Root"
+    case other => other.mkString(",")
+  }
+
+  def main(args: Array[String]): Unit = {
+    val game = Seq(R, R, S, S, S, P, R, R, P, R, S, S, S, P, R, P, P)
+
+    val windowSize = 3
+
+    println(game)
+
+    val slides = game.sliding(windowSize).toSeq
+
+    println(slides)
+
+    val subseqs = slides.flatMap(subSequences).map(ss => (ss.init, ss))
+
+    println(subseqs)
+
+    val edgesGrouped = subseqs.groupBy(identity).map {
+      case ((s, t), es) => WDiEdge(s, t)(es.length)
+    }.toList
+
+    val g = Graph[Node, WDiEdge](edgesGrouped: _*)
+
+    println(g)
+
+    val root = DotRootGraph(directed = true, Some("RPS"))
+
+    def transformer(innerEdge: Graph[Node, WDiEdge]#EdgeT) = innerEdge.edge match {
+      case WDiEdge(source, target, weight) =>
+        Some((root, DotEdgeStmt(showNode(source.toOuter), showNode(target.toOuter), Seq(DotAttr("penwidth", weight), DotAttr("label", weight)))))
+    }
+
+    val dot = g.toDot(root, transformer)
+    println(dot)
+  }
+}
