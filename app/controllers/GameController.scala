ba2d53304f4b162c8e1455012b39b4a09e165dce
yrolland <rolland_yann@orange.fr>
2020-12-05 13:06:21 +0100

    bootstrap vue

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -9,7 +9,7 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class GameController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   /**
    * Create an Action to render an HTML page.
@@ -18,7 +18,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def create() = Action { implicit request: Request[AnyContent] =>
+    Ok("YEP")
   }
 }
---
+++ public/javascripts/main.js
@@ -0,0 +1,8 @@
+Vue.use(VueMaterial.default)
+
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+})
