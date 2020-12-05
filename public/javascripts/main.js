90345d31cc8ea84eb79deebece625446bd4fca72
yrolland <rolland_yann@orange.fr>
2020-12-05 22:11:10 +0100

    wip

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,21 @@
+// const {RPSServiceClient} = require('./generated/rps-service_grpc_web_pb.js');
+// const {PlayTurnRequest} = require('./generated/rps-service_pb.js');
+
 Vue.use(VueMaterial.default)
+
+// const client = new RPSServiceClient('localhost:8080');
 
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    humanScore: 0,
+    aiScore: 0,
+    gameId: null
+  },
+  methods: {
+    play: function(player){
+      this.humanScore++;
+    }
   }
 })
